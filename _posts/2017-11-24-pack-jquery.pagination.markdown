7ec5c1e7a7721e2d9d43c87e061acf3a163b523d
bb595700239 <595700239@qq.com>
2017-11-24 10:59:24 +0800

    pack jquery.pagination.js
    Signed-off-by: bb595700239 <595700239@qq.com>

---
+++ _posts/2017-11-24-pack-jquery.pagination.markdown
@@ -0,0 +1,97 @@
+---
+layout:     post
+title:      "拓展jQuery Pagination分页功能"
+subtitle:   "实用组件"
+date:       2017-11-24
+author:     "Joubn"
+header-img: "img/hello2017.jpg"
+header-mask: 0.3
+catalog: true
+tags:
+    - 前端开发
+    - jquery
+    - 插件
+---
+
+> “Happiness is a direction, not a place. ”
+
+## 介绍说明
+
+原[jquery.pagination.js](https://github.com/gbirke/jquery_pagination)已经很优秀，感谢[MikeGrace](https://github.com/MikeGrace)给我们带来这么好用的轮子！
+
+我的拓展：
+- 原插件CSS不太合理，使用浮动，故无法方便实现左右方向的定位，且未清除浮动，对其DOM进行优化，使其支持text-align的定位。
+- 添加输入页码跳转功能，新增API`isJump`、`jump_format_text`、`jump_outofrange_text`、`jump_null_text`。
+- 对原js文件注释进行了中文翻译，默认文字也是汉化了的
+
+## 参数
+参数名 | 描述 | 参数值 
+--- | :------: | :-----: 
+maxentries | 总条目数 | 必选参数，整数 
+items_per_page | 每页显示的条目数 | 可选参数，默认是10
+num_display_entries | 连续分页主体部分显示的分页条目数 | 可选参数，默认是10
+current_page | 当前选中的页面 | 可选参数，默认是0，表示第1页
+num_edge_entries | 两侧显示的首尾分页的条目数 | 可选参数，默认是0
+link_to | 分页的链接 | 字符串，可选参数，默认是"#"
+prev_text | “前一页”分页按钮上显示的文字 | 字符串参数，可选，默认是"Prev"
+next_text | “下一页”分页按钮上显示的文字 | 字符串参数，可选，默认是"Next"
+ellipse_text | 省略的页数用什么文字表示 | 可选字符串参数，默认是"…"
+prev_show_always | 是否显示“前一页”分页按钮 | 布尔型，可选参数，默认为true，即显示“前一页”按钮
+next_show_always | 是否显示“下一页”分页按钮 | 布尔型，可选参数，默认为true，即显示“下一页”按钮
+callback | 回调函数 | 默认无执行效果
+load_first_page | 初始是否回调 | 布尔型，可选参数，默认为false
+isJump | 显示输入页码跳转 | 布尔型，可选参数，默认为true
+
+## 使用
+
+调用方法：
+
+``` js
+$("#Pagination").pagination(100, {
+    num_edge_entries: 2,
+    num_display_entries: 4,
+    callback: pageselectCallback,
+    items_per_page:1
+});
+```
+
+这段代码表示的含义是：总共有100(maxentries)个列表项，首尾两侧分页显示2(num_edge_entries)个，连续分页主体数目显示4(num_display_entries)个，回调函数为pageselectCallback(callback)，每页显示的列表项为1(items_per_page)。您可以对照参数表修改配置这里的参数。
+
+## demo及下载
+<div id="Pagination-con"></div>
+<div id="Pagination"></div>
+<style>
+.pagination{text-align: center;padding: 15px;height: 32px;overflow: hidden;vertical-align: top;position: relative;z-index:100;}
+.pagination .con{height: 32px;margin: 0 auto;display: inline-block;*zoom: 1;*display: inline;padding: 0;}
+.pagination span.page-num.prev, .pagination span.page-num.next{padding: 0 14px;margin: 0 10px;border-radius: 10px;font-size: 14px;}
+.pagination span{font-size: 14px;float: left;display: inline-block;*zoom: 1;*display: inline;height: 30px;line-height: 30px;}
+.pagination span.page-num{font-size: 14px;padding: 0 10px;border-radius: 10px;border: 1px solid #cccfd2;margin: 0 3px;cursor: pointer;background: #fff;}
+.pagination span.page-num.on{ background: #ffb80d\9; background-image: linear-gradient( 90deg, #ffb80d, #ff960d);color: #fff;border: none;height: 32px;line-height: 32px;}
+.pagination span.next, .pagination span.prev{border: 1px solid #cccfd2;color: #666666;padding: 0 20px;margin: 0 10px;border-radius: 5px;}
+.pagination span.total-info{font-size: 14px;margin-left: 12px;margin-right: 12px;}
+.pagination #jump-index{border: 1px solid #bfbfbf;height: 30px;line-height: 30px;border-radius: 5px;margin: 0 10px;width: 40px;}
+.pagination .jump-page{border: 1px solid #cccfd2;background: #fff;border-radius: 10px;color: #666666;height: 30px;line-height: 30px;width: 64px;cursor: pointer;margin-left: 26px;}
+.pagination-small span{height: 32px;line-height: 32px;}
+.pagination-small span.page-num{padding: 0 9px;}
+.pagination-small #jump-index{height: 32px;line-height: 32px;}
+.pagination-small .jump-page{height: 32px;line-height: 32px;}
+</style>
+
+<script src="{{ '/js/plugin/jquery.pagination.js' | prepend: site.baseurl }}"></script>
+
+<script>
+    $("#Pagination").pagination(100, {
+        num_edge_entries: 2,
+        num_display_entries: 4,
+        callback: function(i){
+            $('#Pagination-con').html(i)  
+        },
+        items_per_page:1,
+        load_first_page:true,
+    });
+</script>
+
+
+
+
+
---
+++ js/plugin/jquery.pagination.js
@@ -0,0 +1,257 @@
+/**
+ * This jQuery plugin displays pagination links inside the selected elements.
+ *
+ * @author Gabriel Birke (birke *at* d-scribe *dot* de)
+ * @version 1.2
+ * @param {int} maxentries Number of entries to paginate
+ * @param {Object} opts Several options (see README for documentation)
+ * @return {Object} jQuery Object
+ */
+jQuery.fn.pagination = function(maxentries, opts){
+
+	opts = jQuery.extend({
+		items_per_page:20,//每页显示的条目数
+		num_display_entries:4,//显示分页条数
+		current_page:0,//默认打开页
+		num_edge_entries:1,//两侧显示的首尾分页的条目数
+		link_to:"#",
+		first_text : "第一页",
+		prev_text:"上一页",
+		next_text:"下一页",
+		last_text : "最后一页",
+		jump_text:"GO",
+		isSum:true,
+		isJump:true,
+		jump_format_text:"页码不正确",
+		jump_outofrange_text:"页码超出范围",
+		jump_null_text:"请输入页码",
+		ellipse_text:"...",
+		first_show_always : true,
+		prev_show_always:true,
+		next_show_always:true,
+		last_show_always : true,
+		load_first_page:false,//第一页是否加载数据
+		callback:function(){return true;}
+	},opts||{});
+	var self = $(this)
+
+	return this.each(function() {
+
+		/**
+		 * Calculate the maximum number of pages
+		 */
+		function numPages() {
+			return Math.ceil(maxentries/opts.items_per_page);
+		}
+		
+		/**
+		 * Calculate start and end point of pagination links depending on 
+		 * current_page and num_display_entries.
+		 * @return {Array}
+		 */
+		function getInterval()  {
+			var ne_half = Math.ceil(opts.num_display_entries/2);
+			var np = numPages();
+			var upper_limit = np-opts.num_display_entries;
+			var start = current_page>ne_half?Math.max(Math.min(current_page-ne_half, upper_limit), 0):0;
+			var end = current_page>ne_half?Math.min(current_page+ne_half, np):Math.min(opts.num_display_entries, np);
+			return [start,end];
+		}
+		
+		/**
+		 * This is the event handling function for the pagination links. 
+		 * @param {int} page_id The new page number
+		 */
+		function pageSelected(page_id, evt){
+			current_page = page_id;
+			drawLinks();
+			var continuePropagation = opts.callback(page_id, panel);
+			if (!continuePropagation) {
+				if (evt.stopPropagation) {
+					evt.stopPropagation();
+				}
+				else {
+					evt.cancelBubble = true;
+				}
+			}
+			return continuePropagation;
+		}
+		
+		/**
+		 * This function inserts the pagination links into the container element
+		 */
+		function drawLinks() {
+			panel.empty();
+			var interval = getInterval();
+			var np = numPages();
+			// This helper function returns a handler function that calls pageSelected with the right page_id
+			var getClickHandler = function(page_id) {
+				return function(evt){ return pageSelected(page_id,evt); }
+			}
+			// Helper function for generating a single link (or a span tag if it's the current page)
+			var appendItem = function(page_id, appendopts){
+				page_id = page_id<0?0:(page_id<np?page_id:np-1); // Normalize page id to sane value
+				appendopts = jQuery.extend({text:page_id+1, classes:""}, appendopts||{});
+				if(page_id == current_page){
+					var lnk = jQuery("<span class='page-num on'>"+(appendopts.text)+"</span>");
+				}
+				else
+				{
+					var lnk = jQuery("<span  class='page-num'>"+(appendopts.text)+"</span>")
+						.bind("click", getClickHandler(page_id))
+
+				}
+				//alert(page_id);
+				if(appendopts.classes){lnk.addClass(appendopts.classes);}
+				panel.append(lnk);
+			}
+			// sum total
+			// if(opts.isSum){
+			// 	panel.append("<span class='total-info'>共&nbsp;"+maxentries+"&nbsp;条记录</span>");
+			// }
+			// Generate "First"-Link
+			if (opts.first_text
+					&& (current_page > 0 || opts.first_show_always)) {
+				
+				appendItem(0,{text:opts.first_text, classes:"first"});  //第一页
+			}
+			// Generate "Previous"-Link
+			if(opts.prev_text && (current_page > 0 || opts.prev_show_always)){
+				appendItem(current_page-1,{text:opts.prev_text, classes:"prev"});
+			}
+			// Generate starting points
+			if (interval[0] > 0 && opts.num_edge_entries > 0)
+			{
+				var end = Math.min(opts.num_edge_entries, interval[0]);
+				for(var i=0; i<end; i++) {
+					appendItem(i);
+				}
+				if(opts.num_edge_entries < interval[0] && opts.ellipse_text)
+				{
+					jQuery("<span>"+opts.ellipse_text+"</span>").appendTo(panel);
+				}
+			}
+			// Generate interval links
+			for(var i=interval[0]; i<interval[1]; i++) {
+				appendItem(i);
+			}
+			// Generate ending points
+			if (interval[1] < np && opts.num_edge_entries > 0)
+			{
+				if(np-opts.num_edge_entries > interval[1]&& opts.ellipse_text)
+				{
+					jQuery("<span>"+opts.ellipse_text+"</span>").appendTo(panel);
+				}
+				var begin = Math.max(np-opts.num_edge_entries, interval[1]);
+				for(var i=begin; i<np; i++) {
+					appendItem(i);
+				}
+			}
+			// Generate "Next"-Link
+			if(opts.next_text && (current_page < np-1 || opts.next_show_always)){
+				appendItem(current_page+1,{text:opts.next_text, classes:"next"});
+			}
+			// Generate "Last"-Link
+			if (opts.last_text
+					&& (current_page < np - 1 || opts.last_show_always)) {
+				appendItem(np,{text:opts.last_text, classes:"last"});//最后一页
+			}
+
+			if(opts.isJump){
+				// Generate Jump Input
+				panel.append(jQuery("<span class='total-info'>共"+numPages()+"页/"+maxentries+"条记录 </span>"));
+				panel.append('<span>跳转到</span>');
+				panel.append(jQuery("<span><input type='text' style='text-align:center;font-weight:bold' class='jump-index' title='请输入正整数' size='1' /></span>"));
+
+				var jumpinput = self.find(".jump-index");
+				// Generate Jump Handler
+				var index = null;
+				var jump = jQuery("<span class='jump-page'>"+opts.jump_text+"</span>").bind("click",function(evt){
+
+					index = jumpinput.val();
+					if(index == null || index == ""){
+						alert(opts.jump_null_text);
+						return;
+					}
+					if(/^\d+$/.test(index)){
+						if(index > numPages() || index < 1){
+							alert(opts.jump_outofrange_text);
+							jumpinput.val("");
+							return;
+						}
+						index-=1;
+						return pageSelected(index,evt);
+					}else{
+						alert(opts.jump_format_text);
+						jumpinput.val("");
+						return;
+					}
+				}).attr("href", opts.link_to.replace(/__id__/,index));
+				panel.append(jump);
+				jumpinput.bind('keyup', function(event){
+					   if (event.keyCode=="13"){
+							index = jumpinput.val();
+							if(index == null || index == ""){
+								alert(opts.jump_null_text);
+								return;
+							}
+							if(/^\d+$/.test(index)){
+								if(index > numPages() || index < 1){
+									alert(opts.jump_outofrange_text);
+									jumpinput.val("");
+									return;
+								}
+								index-=1;
+								
+								return pageSelected(index,event);
+							}else{
+								alert(opts.jump_format_text);
+								jumpinput.val("");
+								return;
+							}
+						  // appendItem(index);
+					   
+					   }
+				
+			      });
+			}
+			
+			
+			
+		}
+		
+		// Extract current_page from options
+		var current_page = opts.current_page;
+		// Create a sane value for maxentries and items_per_page
+		maxentries = (!maxentries || maxentries < 0)?1:maxentries;
+		opts.items_per_page = (!opts.items_per_page || opts.items_per_page < 0)?1:opts.items_per_page;
+		// Store DOM element for easy access from all inner functions
+		var panel = jQuery(this);
+		// Attach control functions to the DOM element 
+		this.selectPage = function(page_id){ pageSelected(page_id);}
+		this.prevPage = function(){ 
+			if (current_page > 0) {
+				pageSelected(current_page - 1);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		this.nextPage = function(){ 
+			if(current_page < numPages()-1) {
+				pageSelected(current_page+1);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		// When all initialisation is done, draw the links
+		drawLinks();
+        // call callback function
+        if(opts.load_first_page) {
+			opts.callback(current_page, this);
+		}
+	});
+}
